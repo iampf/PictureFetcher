0db874ff3160a763d1157dfd7787cce16e2341e5
iampf <iampf510@gmail.com>
2014-10-22 02:15:35 -0400

    remove commom

--- Parser/veryicon.py
+++ Parser/veryicon.py
@@ -55,34 +55,6 @@ class Worker(threading.Thread):
 					break
 
 
-		#for u in urls:
-			
-			
-
-		'''
-		print url
-		category = 'data/stockvault/' + category
-		if not os.path.exists('data'):
-			os.mkdir('data')
-		if not os.path.exists('data/stockvault/'):
-			os.mkdir('data/stockvault/')
-		if not os.path.exists(category):
-			os.mkdir(category)
-
-		html = GetData(url)
-		b = bs(html)
-
-		for item in b.find_all(class_ = 'row_images2'):
-			for img in item.find_all('li'):
-				#img_url = 'http://www.stockvault.net/' + img.a['sample'].split('/.')[1]
-				img_url = 'http://www.stockvault.net' + img.a['sample']
-				file_name = category + '/' + img.a['title'] + '.jpg'
-				if not os.path.exists(file_name):
-
-					StorePicture(img_url, file_name)
-		'''
-
-
 class VeryIcon:
 	def __init__(self, thread_num = 0):
 		self.thread_num = thread_num
@@ -121,25 +93,6 @@ class VeryIcon:
 			for url in c:
 				 self.queue.put( (category[i], 'http://www.veryicon.com' + url['href'], ) )
 
-		'''
-		categories = b.find(class_='categoryitems')
-		
-		urls = []
-		category = []
-		
-		for item in categories.find_all('li'):
-			urls.append( 'http://www.stockvault.net' + item.a['href'])
-			category.append( item.b.get_text())
-
-
-		for i in range(len(urls)):
-			html = GetData(urls[i])
-			b = bs(html)
-			last = int(b.find(class_='paging_bar').find_all('a')[-1]['href'].split('=')[-1])
-			for j in range(last):
-				self.queue.put( (category[i], urls[i]+'/?s=v&p='+str(j+1),))
-		'''
-
 	def getQueue(self):
 		while True:
 			try:
@@ -152,8 +105,3 @@ class VeryIcon:
 if __name__ == '__main__':
 	s = VeryIcon(thread_num=20)
 	s.start()
-	'''
-	s = VeryIcon(thread_num=30)
-	s.getCategory()
-	s.getQueue()
-	'''
